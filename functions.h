6fb4903b813c0dd267623124ea64aeff80fc4620
Bobo <yellow_coffee@mail.ru>
2019-11-13 19:14:22 +0300

    sort by string lexigraphical order

---
+++ functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+std::vector<std::string> split(const std::string &str, char d);
+
+#endif // FUNCTIONS_H
